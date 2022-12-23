c6ef800879b0c0382740552289bdb34895b1b402
selena.fu <selena.fu@pouchen.com>
2022-12-23 16:30:52 +0800

    menu 初版

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <MenuList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MenuList from './components/MenuList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    MenuList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    padding: 0;
+    margin: 0;
 }
 </style>
---
+++ src/components/MenuList.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class = "menu">
+        <div class = "title">Demo Todo List</div>
+        
+        <div v-for="(item,index) in list" :key="item.id">
+            <div class="item">{{index+1}}.{{item.name}}</div>
+        </div>
+        
+        <div>
+            <button class="addBtn">Add Item</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'MenuList',
+  props: {
+  },
+  setup(){
+    const list = [
+        {
+            id:0,
+            name:'item title',
+            content:'text',
+            dateFrom:'',
+            dateEnd:'',
+            image:''
+        }]
+    return{
+        list
+    }
+  }
+
+}
+</script>
+<style lang="scss" scoped>
+.menu {
+    background: #A1FFC7;
+    width: 200px;
+    height: 100vh;
+    .title {
+        padding: 10px;
+        font-family: sans-serif;
+    }
+}
+.item {
+    background: #81F8B1;
+    height: 20px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    font-family: sans-serif;
+}
+.addBtn{
+    background: #E7FFE9;
+    border: #E7FFE9;
+    border-radius: 10px;
+    width: 180px;
+    height: 30px;
+    margin: 10px
+}
+</style>
